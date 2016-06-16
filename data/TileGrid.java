eaba019e5105dd018adfbe9b0480f3da7d787b9b
snkruse <sethkruse@gmail.com>
2016-06-15 22:49:45 -0700

    made Enum TyleType class and started building our 2d tabe

---
+++ data/TileGrid.java
@@ -0,0 +1,43 @@
+package data;
+
+import static helpers.Artist.*;
+
+
+public class TileGrid {
+
+	public Tile[][] map;
+	
+	public TileGrid(){
+		map = new Tile[20][15];
+		for (int i = 0; i < map.length; i++){
+			for(int j = 0; j < map[i].length; j++){
+				map[i][j] = new Tile(i *64, j*64, 64,64, TileType.Grass);
+			}
+		}
+	}
+
+	public TileGrid(int[][] newMap){
+		map = new Tile[20][15];
+		for(int i = 0; i < map.length; i++){
+			for (int j = 0; j < map[i].length; j++){
+				if(newMap[j][i] == 0)
+					map[i][j] = new Tile(i * 64, j * 64, 64, 64, TileType.Grass);
+				else
+					map[i][j] = new Tile(i * 64, j * 64, 64, 64, TileType.Dirt);
+					 
+			}
+		}
+	}
+	
+	public void Draw(){
+		for (int i = 0; i < map.length; i++){
+			for (int j = 0; j < map[i].length; j++){
+				Tile t = map[i][j];
+				DrawQuadTex(t.getTexture(), t.getX(), t.getY(), t.getWidth(), t.getHeight());
+					
+			}
+		}
+	}
+	
+	
+}
---
+++ data/TileType.java
@@ -0,0 +1,18 @@
+package data;
+
+public enum TileType {
+
+	
+	Grass("grass64", true), Dirt("dirt64",false);
+	
+	
+	String textureName;
+	boolean buildable;
+	
+	TileType(String textureName, boolean buildable){
+		this.textureName = textureName;
+		this.buildable = buildable;
+		
+	}
+	
+}
