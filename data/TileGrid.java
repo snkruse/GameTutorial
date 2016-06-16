5cd7f0ee48dcb863cee34b7b9ed4b5e613d08b1f
snkruse <sethkruse@gmail.com>
2016-06-15 23:35:53 -0700

    added water tile

--- data/TileGrid.java
+++ data/TileGrid.java
@@ -19,15 +19,32 @@ public class TileGrid {
 	public TileGrid(int[][] newMap){
 		map = new Tile[20][15];
 		for(int i = 0; i < map.length; i++){
-			for (int j = 0; j < map[i].length; j++){
-				if(newMap[j][i] == 0)
+			for (int j = 0; j < map[i].length; j++){				
+				switch(newMap[j][i]){
+				case 0:
 					map[i][j] = new Tile(i * 64, j * 64, 64, 64, TileType.Grass);
-				else
+					break;
+				case 1:
 					map[i][j] = new Tile(i * 64, j * 64, 64, 64, TileType.Dirt);
-					 
+					break;
+				case 2:
+					map[i][j] = new Tile(i * 64, j * 64, 64, 64, TileType.Water);
+					break;
+				}
+				
 			}
 		}
 	}
+	
+	public void SetTile(int xCoord, int yCoord, TileType type)
+	{
+		map[xCoord][yCoord] = new Tile(xCoord * 64, yCoord * 64, 64, 64, type);
+	}
+	
+	public Tile getTile(int xCoord, int yCoord){
+		return map[xCoord][yCoord];
+	}
+	
 	
 	public void Draw(){
 		for (int i = 0; i < map.length; i++){
